b99da21f7657685a519f3338dc7cc930065d4f5d
DUDEKULA JAINABEE <125980065+jainabee248@users.noreply.github.com>
2025-05-21 16:55:12 +0530

    Add files via upload

---
+++ java/hello.java
@@ -0,0 +1,18 @@
+
+public class hello {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//System.out.println("hello world");
+		String name ="jainabee";
+		System.out.println(name);
+		name="jaina";
+		System.out.println(name);
+		System.out.println(name.length());
+		System.out.println(name.toUpperCase());
+		String[] studentStrings = new String[10];
+		System.out.println(studentStrings.length);
+
+	}
+
+}
